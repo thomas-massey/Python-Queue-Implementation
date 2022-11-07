972825e6576d222e83befb2f13e17d17022f34ee
Leftmostchain21 <thomassey476@outlook.com>
2022-11-07 09:20:58 +0000

    Boilerplate

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a simple python implemetation of a Queue
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, data):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def is_empty():
+        pass
+
+    def peek():
+        pass
+
+    def size():
+        pass
